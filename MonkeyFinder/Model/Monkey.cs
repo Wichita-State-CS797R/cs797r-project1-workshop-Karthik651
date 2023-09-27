dca85969e2fee921384e5049be00fa0c69293197
Karthik Garidepalli <garidepallikarthik23@gmail.com>
2023-09-27 12:28:05 -0500

    Commit
    Commit

---
+++ MonkeyFinder/Model/Monkey.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MonkeyFinder.Model;
+
+public class Monkey
+{
+    public string Name { get; set; }
+
+    public string Age { get; set; }
+
+    public string Location { get; set; }
+}
