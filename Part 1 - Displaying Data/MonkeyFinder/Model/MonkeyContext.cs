3edb35b5a4c68f02456738ea2af1a8e9373b093f
Karthik <garidepallikarthik23@gmail.com>
2023-09-29 18:34:26 -0500

    monkey context class

---
+++ Part 1 - Displaying Data/MonkeyFinder/Model/MonkeyContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MonkeyFinder.Model;
+
+[JsonSerializable(typeof(List<Monkey>))]
+internal sealed partial class MonkeyContext : JsonSerializerContext
+{
+
+}
