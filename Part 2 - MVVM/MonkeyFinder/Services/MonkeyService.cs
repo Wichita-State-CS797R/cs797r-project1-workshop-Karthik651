cbc6b6936c9cdb864c47489f58c90e086a3c7cae
Karthik <garidepallikarthik23@gmail.com>
2023-09-29 19:04:23 -0500

    money service part 2

--- Part 2 - MVVM/MonkeyFinder/Services/MonkeyService.cs
+++ Part 2 - MVVM/MonkeyFinder/Services/MonkeyService.cs
@@ -1,5 +1,33 @@
-﻿namespace MonkeyFinder.Services;
+﻿using System.Net.Http.Json;
+
+namespace MonkeyFinder.Services;
 
 public class MonkeyService
 {
+    HttpClient httpClient;
+    public MonkeyService()
+    {
+        this.httpClient = new HttpClient();
+    }
+
+    List<Monkey> monkeyList = new();
+    public async Task<List<Monkey>> GetMonkeys()
+    {
+        if (monkeyList?.Count > 0)
+            return monkeyList;
+
+        var response = await httpClient.GetAsync("https://www.montemagno.com/monkeys.json");
+
+        if (response.IsSuccessStatusCode)
+        {
+            monkeyList = await response.Content.ReadFromJsonAsync(MonkeyContext.Default.ListMonkey);
+        }
+
+        using var stream = await FileSystem.OpenAppPackageFileAsync("monkeydata.json");
+        using var reader = new StreamReader(stream);
+        var contents = await reader.ReadToEndAsync();
+        monkeyList = JsonSerializer.Deserialize(contents, MonkeyContext.Default.ListMonkey);
+
+        return monkeyList;
+    }
 }
